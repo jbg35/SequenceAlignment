084b87410d93fa1d13750872d2722bb064efdd44
jake <jbg35@zips.uakron.edu>
2019-03-14 17:38:44 -0400

    Comments and stuff

--- seq_align.py
+++ seq_align.py
@@ -36,25 +36,53 @@ def align(seq1, seq2):
     #    (n + 1) * (m + 1).
     # 3: Fill the scoring matrix using the equation below
     #    The max of the diagonals, left adjacent and right adjacent
+    # 4: Traceback in the matrix. starting at the element with the highest
+    #    score, traceback based on the source of each score reursively,
+    #    until 0 is encountered
 
-    # 2:
+    ## NOTE: 1: Determine gap penalty scheme.
+    ##          * s(a,b) - similarity score of the elements that
+    ##            constituted the two sequences
+    ##          * W_k - the penalty of a gap that has length k
+    # GLOBAL VARIABLES AT THE TOP
+    # match       = 3
+    # mismatch    = -3
+    # gap         = -2
+
+    ## NOTE: 2: & 3: creates a scoring matrix and fills according to
+    ##               match, mismatch, and gap scoring. Checks the max
+    ##               from the diagonal, left_adj, and up_adj. If none
+    ##               are above 0 fill current spot in matrix with 0.
     matrix = [[0 for x in range(seq2len+1)] for y in range(seq1len+1)]
     max_score = 0
     max_pos = None
     for i in range(1, seq1len+1):
         for j in range(1,seq2len+1):
-            score  = calc_score(matrix,i,j)
+            score  = score_matrix(matrix,i,j)
             if score > max_score:
                 max_score = score
                 max_pos = (i,j)
             matrix[i][j] = score
 
+    ## NOTE: 4: Traceback in the matrix to find best alignment.
+    ##          Start at element with highest score, traceback based on the
+    ##          source of each score recursively, until 0 is encountered. The
+    ##          segments that have the highest similarity score based on the
+    ##          given scoring system is generated in this process. To obtain
+    ##          the second best local alignment, apply the traceback process
+    ##          starting at the second highest score outside the trace of the
+    ##          best alignment.
+    ## TODO: TRACEBACK
+
+
     #prints matrix all clean like
     print('        ', ''.join(['{:5}'.format(item) for item in seq2]))
+    ## TODO: Left side of matrix letters.
     print('\n'.join([''.join(['{:5}'.format(item) for item in row]) for row in matrix]))
+
     return matrix, max_pos
 
-def calc_score(matrix, x, y):
+def score_matrix(matrix, x, y):
     similarity = match if seq1[x-1] == seq2[y-1] else mismatch
     diag = matrix[x-1][y-1] + similarity
     up_adj = matrix[x-1][y] + gap
