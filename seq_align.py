c0754a622b9880d37ce47c40167b5a0770692017
Jake <jake@localhost.localdomain>
2019-03-17 12:34:29 -0400

    Tracebackb

--- seq_align.py
+++ seq_align.py
@@ -56,6 +56,7 @@ def align(seq1, seq2):
     matrix = [[0 for x in range(seq2len+1)] for y in range(seq1len+1)]
     max_score = 0
     max_pos = None
+
     for i in range(1, seq1len+1):
         for j in range(1,seq2len+1):
             score  = score_matrix(matrix,i,j)
@@ -63,7 +64,15 @@ def align(seq1, seq2):
                 max_score = score
                 max_pos = (i,j)
             matrix[i][j] = score
+    print(matrix[7][6])
+    #prints matrix all clean like
+    print('        ', ''.join(['{:5}'.format(item) for item in seq2]))
+    ## TODO: left letters maybe???
+    print('\n'.join([''.join(['{:5}'.format(item) for item in row]) for row in matrix]))
 
+    return matrix, max_pos
+
+def traceback(matrix, maxpos):
     ## NOTE: 4: Traceback in the matrix to find best alignment.
     ##          Start at element with highest score, traceback based on the
     ##          source of each score recursively, until 0 is encountered. The
@@ -72,15 +81,83 @@ def align(seq1, seq2):
     ##          the second best local alignment, apply the traceback process
     ##          starting at the second highest score outside the trace of the
     ##          best alignment.
-    ## TODO: TRACEBACK
+    ##
+    ## Stepper looks for max(up,left,diagonal)
+    ##
+    ## while(0 is not found)
+    ##  if stepper => diagonal: match/mismatch
+    ##  elif stepper => up: gap in seq1
+    ##  elif stepper => left: gap in seq2
+    align1 = []
+    align2 = []
+    (x,y) = maxpos
+    stepper = next_step(matrix, x, y)
+    while(stepper != 0):
+        # Keep char in string if we traverse diagonaly
+        print('step:', stepper)
+        if stepper == 1:
+            align1.append(seq1[x-1])
+            align2.append(seq2[y-1])
+            x -= 1
+            y -= 1
+        elif stepper == 2:
+            align1.append('-')
+            align2.append(seq2[y-1])
+            y -= 1
+        elif stepper == 3:
+            align1.append(seq1[x-1])
+            align2.append('-')
+            x -= 1
+        # print(align1)
+        # print()
+        # print(align2)
+        stepper = next_step(matrix, x, y)
+
+        print('step after:', stepper)
 
 
-    #prints matrix all clean like
-    print('        ', ''.join(['{:5}'.format(item) for item in seq2]))
-    ## TODO: left letters maybe???
-    print('\n'.join([''.join(['{:5}'.format(item) for item in row]) for row in matrix]))
+    align1.append(seq1[x-1])
+    align2.append(seq2[y-1])
+    # Initially reversed because we are working backward on the string
+    print(align1)
+    print(align2)
+    print('\n')
 
-    return matrix, max_pos
+    align1 = align1[::-1]
+    align2 = align2[::-1]
+
+    formatter = []
+    for i in range(len(align1)):
+        if align1[i] == align2[i]:
+            formatter.append('|')
+        elif (align1[i] or align2[i]) == '-':
+            formatter.append(' ')
+        elif align1[i] != align2[i]:
+            formatter.append(':')
+
+    print(' '.join(align1))
+    print(' '.join(formatter))
+    print(' '.join(align2))
+
+    return 0
+
+def next_step(matrix, x, y):
+    diag =  matrix[x-1][y-1]
+    up   =  matrix[x][y-1]
+    left =  matrix[x-1][y]
+    print(diag, ' ', up, ' ', left)
+    maxstep = max(diag,up,left)
+    if (diag and up and left) == 0:
+        print('END')
+        return 0
+
+    if diag == maxstep:
+        return 1 
+    if up == maxstep:
+        return 2 
+    if left == maxstep:
+        return 3 
+    assert False
 
 def score_matrix(matrix, x, y):
     # Scores the matrix. Starts at top left of matrixand works its way
@@ -91,15 +168,15 @@ def score_matrix(matrix, x, y):
     # if seq1[a] == seq2[b]
     #   then: match
     #   else: mismatch
-    diag = matrix[x-1][y-1] + (match if seq1[x-1] == seq2[y-1] else mismatch)
-
-    up_adj = matrix[x-1][y] + gap
+    diag     = matrix[x-1][y-1] + (match if seq1[x-1] == seq2[y-1] else mismatch)
+    up_adj   = matrix[x-1][y] + gap
     left_adj = matrix[x][y-1] + gap
     return max(0, diag, up_adj, left_adj)
 
 if __name__ == "__main__":
 
-    seq1 = 'ggttgacta'
-    seq2 = 'tgttacgg'
+    seq1 = 'tgttacgg'
+    seq2 = 'ggttgacta'
 
-    align(seq1, seq2)
+    matrix, maxpos = align(seq1, seq2)
+    traceback(matrix, maxpos)
